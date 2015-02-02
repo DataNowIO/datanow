35460a80ca0a855ddc62f1fc500beaacf1f95c3d
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2015-02-02 15:41:32 +1000

    Cleanup

--- tests/bad-test.sh
+++ tests/bad-test.sh
@@ -6,16 +6,24 @@ MY_PATH="`dirname \"$0\"`"
 
 rm -f ~/.datanow-config.json
 
-DataNow = "node $MY_PATH/../bin/cmd.js --server http://localhost:3000  --loglevel debug"
+DataNow="node $MY_PATH/../bin/cmd.js --server http://localhost:3000  --loglevel debug"
 
-$DataNow --register --username garrows --email glen.arrowsmith@gmail.com --password g --login
 
-$DataNow --newApp testApp
 
-$DataNow --newBoard testBoard --app testApp
 
-$DataNow --app testApp --board testBoard --write 1
-$DataNow --app testApp --board testBoard --write 2
-$DataNow --app testApp --board testBoard --write 3
+$DataNow register --username garrows --email glen.arrowsmith@gmail.com --password g --noLogin
+$DataNow login --username garrows --email glen.arrowsmith@gmail.com --password g
 
-$DataNow --app testApp --board testBoard --read
+$DataNow create testApp
+$DataNow create testApp/testBoard
+
+# $DataNow write 1
+# $DataNow use testApp/testBoard
+# $DataNow write 2
+$DataNow write --board testApp/testBoard 1
+$DataNow write --board testApp/testBoard 2
+$DataNow write --board testApp/testBoard 3
+
+# $DataNow read
+$DataNow read --board testApp/testBoard
+say winning
