7c9e31ba8738813c597b601ddc67a24bc35b7884
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2015-03-13 17:05:31 +1000

    Got bad tests working

--- tests/bad-test.sh
+++ tests/bad-test.sh
@@ -13,59 +13,58 @@ datanow set --loglevel debug
 
 
 
-datanow register --username garrows --email glen.arrowsmith@gmail.com --password g
-datanow register --username garrows2 --email glen@datanow.io --password g2
+datanow register --username homer --email glen+homer@datanow.io --password password1
+datanow register --username marge --email glen+marge@datanow.io --password password2
 
-datanow login --username garrows --email glen.arrowsmith@gmail.com --password g
+datanow login --username homer --email glen+homer@datanow.io --password password1
 
 
-datanow create garrows/test-board-up
-datanow collaborators garrows/test-board-up --add garrows2
-datanow collaborators garrows/test-board-up --list
+datanow create homer/test-board-up
+datanow create homer/test-board-down
+datanow collaborators homer/test-board-up --add marge
+datanow collaborators homer/test-board-down --add marge
+datanow collaborators homer/test-board-up --list
 
-datanow create garrows/test-board-down
 
 
 datanow write 9
-datanow set --board garrows/test-board-up
+datanow set --board homer/test-board-up
 datanow write 1
-datanow write --board garrows/test-board-up 2
-datanow write --board garrows/test-board-down `date -u +"%Y-%m-%dT%H:%M:%SZ"` 8
-datanow write --board garrows/test-board-up 3
+datanow write --board homer/test-board-up 2
+datanow write --board homer/test-board-down `date -u +"%Y-%m-%dT%H:%M:%SZ"` 8
+datanow write --board homer/test-board-up 3
 
 datanow logout
-exit 1
-# datanow login --email glen@datanow.io --password g2
-# datanow write --board garrows/test-board-down 7
-#
-#
-# datanow create garrows/testBoard4
-# datanow create testApp3
-# datanow create testApp3/testBoard3 number number string date
-# datanow collaborator testApp3/testBoard3 --add garrows
-# datanow set --board testApp3/testBoard3
-#
-# datanow write 1 2 chicken `date -u +"%Y-%m-%dT%H:%M:%SZ"`
-# datanow logout
-# datanow login --email glen.arrowsmith@gmail.com --password g
-# datanow write 3 4 duck `date -u +"%Y-%m-%dT%H:%M:%SZ"`
-#
-datanow create garrows/singleNumbers number
+
+datanow login --email glen+marge@datanow.io --password password2
+datanow write --board homer/test-board-down 7
+
+
+datanow create marge/test-board-nnsd number number string date
+datanow collaborators marge/test-board-nnsd --add homer
+datanow set --board marge/test-board-nnsd
+
+datanow write 1 2 chicken `date -u +"%Y-%m-%dT%H:%M:%SZ"`
+datanow logout
+datanow login --email glen+homer@datanow.io --password password1
+datanow write 3 4 duck `date -u +"%Y-%m-%dT%H:%M:%SZ"`
+
+datanow create homer/singleNumbers number
 
 for i in {0..5}; do   datanow write -- $i; done
 
-datanow set --board garrows/test-board-up --loglevel info
+datanow set --board homer/test-board-up --loglevel info
 
-datanow read --board garrows/singleNumbers --limit 3
-datanow read --board garrows/singleNumbers --limit 3 --page 2
+datanow read --board homer/singleNumbers --limit 3
+datanow read --board homer/singleNumbers --limit 3 --page 2
 
 datanow read
-#
-# datanow read --board garrows/test-board-down
-#
-# datanow read --board testApp3/testBoard3
-#
-datanow read --board garrows/singleNumbers --format plot
+
+datanow read --board homer/test-board-down
+
+datanow read --board marge/test-board-nnsd
+
+datanow read --board homer/singleNumbers --format plot
 
 
 
